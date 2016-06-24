6f28c984576e3bb702e5f090b3033bb46ccae498
Alexander Tabakov <alex.daymon@gmail.com>
2016-06-24 15:21:17 +0500

    initial

---
+++ README.md
@@ -0,0 +1,14 @@
+# Yii2 extension implementing authorization chain
+
+## What it is for?
+
+For applications with hierarhical access roles system.
+
+## Any examples? It's not clear
+
+You have an application - API. You have two roles here:
+    - admin - they can watch statistics and edit clients;
+    - client - they can see own statistics and set settings.
+
+You need to allow admins to authorize as clients without knowing their passwords just from admin interface. 
+And you need to allow admins to get back to their account without re-logining. And of course you need to log all changes made admins as clients.
---
+++ src/Service.php
@@ -0,0 +1,149 @@
+<?php
+
+namespace Daydiff\AuthChain;
+
+use Yii;
+use yii\base\Component;
+use yii\web\Session;
+
+/**
+ * Description of Service
+ *
+ * @author aleksandr.tabakov
+ */
+class Service extends Component
+{
+    public $sessionKey;
+
+    /**
+     * Adds member to an authorization chain
+     * @param Member $member
+     */
+    public function add(Member $member)
+    {
+        $chain = $this->getChain();
+
+        if (!is_array($chain)) {
+            $chain = [];
+        }
+
+        $chain[] = serialize($member);
+        $this->setChain($chain);
+    }
+
+    /**
+     * Returns first member
+     * @return null|Member
+     */
+    public function first()
+    {
+        $chain = $this->getChain();
+
+        if (!is_array($chain) || !count($chain)) {
+            return null;
+        }
+
+        $first = unserialize($chain[0]);
+        return $first;
+    }
+
+    /**
+     * Returns last member
+     * @return null|Member
+     */
+    public function last()
+    {
+        $chain = $this->getChain();
+
+        if (!is_array($chain) || !count($chain)) {
+            return null;
+        }
+
+        $last = unserialize(array_pop($chain));
+        return $last;
+    }
+
+    /**
+     * Returns the last member and remove it off the chain
+     * @return null|Member
+     */
+    public function pop()
+    {
+        $chain = $this->getChain();
+
+        if (!is_array($chain)) {
+            return null;
+        }
+
+        $member = unserialize(array_pop($chain));
+        $this->setChain($chain);
+
+        return $member;
+    }
+
+    /**
+     * Returns all authorization members
+     * @return Member[]
+     */
+    public function all()
+    {
+        $chain = $this->getChain();
+
+        if (!is_array($chain)) {
+            return [];
+        }
+
+        foreach ($chain as $key => &$value) {
+            $chain[$key] = unserialize($value);
+        }
+
+        return $chain;
+    }
+
+    /**
+     * Returns count of an authorization members
+     * @return integer
+     */
+    public function count()
+    {
+        $chain = $this->getChain();
+
+        return is_array($chain) ? count($chain) : 0;
+    }
+
+    /**
+     * Clears an authorization chain
+     */
+    public function flush()
+    {
+        $session = Yii::$app->get('session', false);
+
+        if (!$session) {
+            return;
+        }
+
+        $session->remove($this->sessionKey);
+    }
+
+    private function getChain()
+    {
+        $session = Yii::$app->get('session', false);
+
+        if (!$session) {
+            return null;
+        }
+
+        $session->get($this->sessionKey, 0);
+    }
+
+    private function setChain($chain)
+    {
+        $session = Yii::$app->get('session', false);
+
+        if (!$session) {
+            return;
+        }
+
+        $session->set($this->sessionKey, $chain);
+    }
+}
